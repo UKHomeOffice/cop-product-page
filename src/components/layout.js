950d9298e019d14729e2f3a6d939dcc012b89e4a
Dayam Javed <dayam.javed@digital.homeoffice.gov.uk>
2020-02-28 18:04:15 +0000

    initial dev commit

---
+++ src/components/layout.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
+
+import Header from './header'
+import './layout.css'
+import Footer from "./footer";
+
+const Layout = ({ path, children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Header currentPage={path} />
+        <div
+          className="govuk-main-wrapper govuk-width-container main-height"
+        >
+          {children}
+        </div>
+        <Footer/>
+      </>
+    )}
+  />
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
