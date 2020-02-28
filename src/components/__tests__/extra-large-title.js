bf0137f9ee286b6abc899b2fbce4cebc8c0d46dd
Dayam Javed <dayam.javed@digital.homeoffice.gov.uk>
2020-02-28 14:46:53 +0000

    unit tests

--- src/components/__tests__/extra-large-title.js
+++ src/components/__tests__/extra-large-title.js
@@ -1,11 +1,19 @@
-import React from "react"
-import renderer from "react-test-renderer"
+import React from "react";
+import renderer from "react-test-renderer";
 import ExtraLargeTitle from "../extra-large-title";
+
 describe("extra-large-title", () => {
   it("renders correctly", () => {
     const tree = renderer
-      .create(<ExtraLargeTitle text={'a title'} />)
+      .create(<ExtraLargeTitle/>)
       .toJSON();
-    expect(tree).toMatchSnapshot()
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("Returns the correct title 'text' prop passed in", () => {
+    const tree = renderer
+      .create(<ExtraLargeTitle text={"a title"}/>);
+    const testInstance = tree.root;
+    expect(testInstance.props.text).toBe("a title");
   });
 });
--- src/components/__tests__/header.js
+++ src/components/__tests__/header.js
@@ -8,4 +8,18 @@ describe("Header", () => {
       .toJSON();
     expect(tree).toMatchSnapshot()
   });
+
+  it("displays the service name", () => {
+    const tree = renderer
+      .create(<Header/>);
+    const testInstance = tree.root;
+    expect(testInstance.findByProps({className: "govuk-header__product-name"}).children).toEqual(['Central Operations Platform']);
+  });
+
+  it("displays the GOV.UK text/ logo", () => {
+    const tree = renderer
+      .create(<Header/>);
+    const testInstance = tree.root;
+    expect(testInstance.findByProps({className: "govuk-header__logotype-text"}).children).toEqual(['GOV.UK']);
+  });
 });
