7addf39a2d9fda05cf1baa8c0ba6a5d41a87bb38
Dayam Javed <dayam.javed@digital.homeoffice.gov.uk>
2020-03-03 16:36:55 +0000

    href

--- src/components/phase-banner.js
+++ src/components/phase-banner.js
@@ -7,7 +7,7 @@ const PhaseBanner = () => (
         beta
       </strong>
       <span className="govuk-phase-banner__text">
-      This is a new service – your <a className="govuk-link" href="#">feedback</a> will help us to improve it.
+      This is a new service – your <a className="govuk-link" href="https://gov.uk">feedback</a> will help us to improve it.
     </span>
     </p>
   </div>
