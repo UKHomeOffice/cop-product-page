498c81dae9ea5bd79c90f14da0d5e2cc0a04fe9d
Dayam Javed <dayam.javed@digital.homeoffice.gov.uk>
2020-03-09 14:52:44 +0000

    nav and sub nav highlight fix for internal and inbound redirect

--- src/components/sub_navigation.js
+++ src/components/sub_navigation.js
@@ -3,11 +3,11 @@ import { Link } from "gatsby";
 
 const SubNavigation = (props) => {
   const navItems = [
-    { name: "About COP", href: "/about/cop" },
-    { name: "Benefits of COP", href: "/about/benefits" },
-    { name: "How to use COP", href: "/about/accessing" },
-    { name: "Roadmap", href: "/about/roadmap" },
-    { name: "Security", href: "/about/security" }
+    { name: "About COP", href: "/about/" },
+    { name: "Benefits of COP", href: "/about/benefits/" },
+    { name: "How to use COP", href: "/about/accessing/" },
+    { name: "Roadmap", href: "/about/roadmap/" },
+    { name: "Security", href: "/about/security/" }
   ];
   return (<>
       <div>
@@ -17,12 +17,9 @@ const SubNavigation = (props) => {
               return <li
                 className="sub-navigation__item"
                 key={index}>
-                <div
-                  className={props.currentPage === `${value.href}` ? "sub-navigation__item--active" : ""}>
-                  <Link style={{ color: "black" }} className="govuk-link subnav" to={value.href}>
+                  <Link activeClassName="sub-navigation__item--active" style={{ color: "black" }} className="govuk-link subnav" to={value.href}>
                     <span>{value.name}</span>
                   </Link>
-                </div>
               </li>;
             })}
           </ol>
