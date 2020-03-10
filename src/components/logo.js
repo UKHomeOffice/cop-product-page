d84b0a93b21ef78f66d54d91c26d1982c3f35de7
Dayam Javed <36885951+d-javed@users.noreply.github.com>
2020-03-10 15:04:12 +0000

    Feature/cp 50/landing page v2 (#15)
    * content change for homepage
    
    * logo
    
    * links update
    
    * beta removal
    
    Co-authored-by: Dayam Javed <dayam.javed@digital.homeoffice.gov.uk>

---
+++ src/components/logo.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+const Logo = (props) => (
+  <img style={{marginBottom: '-4px'}} src={props.source} height={"30px"} width={"49px"} alt={props.alt}/>
+);
+export default Logo
+
