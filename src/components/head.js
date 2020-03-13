c10ed41f2cdd41157f51cd9bf5f7bcd74aee0364
Dayam Javed <36885951+d-javed@users.noreply.github.com>
2020-03-13 16:27:38 +0000

    favicon import (#21)
    * favicon import
    
    * remove move beta
    
    * add beta
    
    Co-authored-by: Dayam Javed <dayam.javed@digital.homeoffice.gov.uk>

--- src/components/head.js
+++ src/components/head.js
@@ -1,24 +1,33 @@
-import React from "react"
+import React from "react";
 import { Helmet } from "react-helmet";
 
 const Head = () => (
   <Helmet>
     <meta charSet="utf-8"/>
-    <title>GOV.UK - Central Operations Platform</title>
+    <title>Central Operations Platform</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
     <meta name="theme-color" content="blue"/>
 
     <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
 
-    <link rel="shortcut icon" sizes="16x16 32x32 48x48" href="/images/favicon.ico" type="image/x-icon"/>
-    <link rel="mask-icon" href="/images/govuk-mask-icon.svg" color="blue"/>
-    <link rel="apple-touch-icon" sizes="180x180" href="/images/govuk-apple-touch-icon-180x180.png"/>
-    <link rel="apple-touch-icon" sizes="167x167" href="/images/govuk-apple-touch-icon-167x167.png"/>
-    <link rel="apple-touch-icon" sizes="152x152" href="/images/govuk-apple-touch-icon-152x152.png"/>
-    <link rel="apple-touch-icon" href="/images/govuk-apple-touch-icon.png"/>
-    {/*<link href="/stylesheets/govuk-frontend-3.5.0.min.css" rel="stylesheet"/>*/}
-    <body className="js-enabled govuk-template__body govuk-main-wrapper--auto-spacing " />
+    <link rel="apple-touch-icon" sizes="57x57" href="/images/apple-icon-57x57.png"/>
+    <link rel="apple-touch-icon" sizes="60x60" href="/images/apple-icon-60x60.png"/>
+    <link rel="apple-touch-icon" sizes="72x72" href="/images/apple-icon-72x72.png"/>
+    <link rel="apple-touch-icon" sizes="76x76" href="/images/apple-icon-76x76.png"/>
+    <link rel="apple-touch-icon" sizes="114x114" href="/images/apple-icon-114x114.png"/>
+    <link rel="apple-touch-icon" sizes="120x120" href="/images/apple-icon-120x120.png"/>
+    <link rel="apple-touch-icon" sizes="144x144" href="/images/apple-icon-144x144.png"/>
+    <link rel="apple-touch-icon" sizes="152x152" href="/images/apple-icon-152x152.png"/>
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-icon-180x180.png"/>
+    <link rel="icon" type="image/png" sizes="192x192" href="/images/android-icon-192x192.png"/>
+    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+    <link rel="icon" type="image/png" sizes="96x96" href="/images/favicon-96x96.png"/>
+    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
+    <link rel="manifest" href="/images/manifest.json"/>
+    <meta name="msapplication-TileImage" content="/images/ms-icon-144x144.png"/>
+
+    <body className="js-enabled govuk-template__body govuk-main-wrapper--auto-spacing "/>
   </Helmet>
 );
 
-export default Head
+export default Head;
