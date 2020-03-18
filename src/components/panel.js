ff04dbbf90991689b5f0c403bfe454d6a6e6c6a3
Dayam Javed <dayam.javed@digital.homeoffice.gov.uk>
2020-03-18 15:20:46 +0000

    content changes

--- src/components/panel.js
+++ src/components/panel.js
@@ -3,29 +3,33 @@ import ExtraLargeTitle from "./extra-large-title";
 import Image from "./image";
 import LargeParagraph from "./large-paragraph";
 import { Link } from "gatsby";
+import SubPanel from "./sub-panel";
 
 const Panel = (props) => (
-  <div className="govuk-panel">
-    <div className="govuk-width-container" style={{ marginTop: "30px" }}>
-      <div className="govuk-grid-row">
-        <div className="govuk-grid-column-two-thirds">
-          <ExtraLargeTitle isPanel={true} text={props.title}/>
-          <LargeParagraph isPanel={true} text={props.paragraphText}/>
-          <div className="button-container" style={{marginBottom: '20px'}}>
-            <Link to="/about/" role="button" draggable="false"
-               className="govuk-button product-page-button button-container__button govuk-!-margin-right-3">
-              Find out more
-            </Link>
-            or &nbsp;<a  style={{color: 'white'}} className="govuk-link" href="https://www.cop.homeoffice.gov.uk/">sign in to COP if you want to use it</a>
+  <>
+    <div className="govuk-panel">
+      <div className="govuk-width-container" style={{ marginTop: "30px" }}>
+        <div className="govuk-grid-row">
+          <div className="govuk-grid-column-two-thirds">
+            <ExtraLargeTitle isPanel={true} text={props.title}/>
+            <LargeParagraph isPanel={true} text={props.paragraphText}/>
+            <div className="button-container" style={{ marginBottom: "20px" }}>
+              <Link to="/about/" role="button" draggable="false"
+                    className="govuk-button product-page-button button-container__button govuk-!-margin-right-3">
+                Find out more
+              </Link>
+              or &nbsp;<a style={{ color: "white" }} className="govuk-link" href="https://www.cop.homeoffice.gov.uk/">sign
+              in to COP if you want to use it</a>
+            </div>
           </div>
-        </div>
-        <div className="govuk-grid-column-one-third">
-          <Image source={props.imageSource} alt={props.imageAlt}/>
+          <div className="govuk-grid-column-one-third">
+            <Image source={props.imageSource} alt={props.imageAlt}/>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-
+    <SubPanel/>
+  </>
 );
 
 export default Panel;
