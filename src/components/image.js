80b4eda5494ac52a5b4ae18ee76fab74a6785352
Dayam Javed <dayam.javed@digital.homeoffice.gov.uk>
2020-03-26 18:54:11 +0000

    remove redundant attribute

--- src/components/image.js
+++ src/components/image.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 
 const Image = (props) => (
-  <img id={props.id} style={{maxWidth: '100%'}} src={props.source} alt={props.alt}/>
+  <img style={{maxWidth: '100%'}} src={props.source} alt={props.alt}/>
 );
 export default Image
 
