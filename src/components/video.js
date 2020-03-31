516b3ce884829d6a1fadf20094b1428f951c18d6
Dayam Javed <dayam.javed@digital.homeoffice.gov.uk>
2020-03-31 13:48:42 +0100

    potential fix for video

--- src/components/video.js
+++ src/components/video.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const Video = (props) => (
   <div className="responsive-video">
-    <video width={props.width} height={props.height} controls>
+    <video width={props.width} height={props.height} controls playsInline>
       <source src={props.source} type="video/mp4"/>
     </video>
   </div>
