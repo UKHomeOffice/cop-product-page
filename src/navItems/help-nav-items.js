e498f6e85d68adb99f6fba63882d5f0a94bfd0f7
Dayam Javed <dayam.javed@digital.homeoffice.gov.uk>
2020-04-29 18:53:53 +0100

    new pages

--- src/navItems/help-nav-items.js
+++ src/navItems/help-nav-items.js
@@ -6,11 +6,11 @@ export const navItems = [
       { name: "Checking your OAR Submission", href: "/help/oar/checking/" }]
   },
   {
-    name: "blah", href: "/help/blah/#OARoverview",
-    subItems: [{ name: "Overview", href: "/help/oar/#OARoverview" },
-      { name: "Finding the OAR form", href: "/help/oar/#findingOAR" },
-      { name: "Entering time and shift details", href: "/help/oar/#enteringTimeShiftsDetails" },
-      { name: "Recording details of your activty", href: "/help/oar/#recordingDetailsActivity" },
-      { name: "Checking your OAR submission ", href: "/help/oar/#checkingOARSubmission" }]
+    name: "Mandatory Declaration", href: "/help/mandatory-declaration/overview/",
+    subItems: [{ name: "Overview", href: "/help/mandatory-declaration/overview/" }]
+  },
+  {
+    name: "Immediate Event Notifications", href: "/help/immediate-event-management/overview/",
+    subItems: [{ name: "Overview", href: "/help/immediate-event-management/overview/" }]
   }
 ];
