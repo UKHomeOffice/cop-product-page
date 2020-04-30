25d60929e4278be63674fc0f5b34f00264e7c758
Dayam Javed <dayam.javed@digital.homeoffice.gov.uk>
2020-04-30 16:16:54 +0100

    changes

--- src/components/sub_navigation.js
+++ src/components/sub_navigation.js
@@ -1,20 +1,17 @@
 import React from "react";
 import { Link } from "gatsby";
 
-let a;
 let gIndex;
 const displaySubNav = (value, index) => {
-  const location = window.location;
-  console.log(value.href, "href");
-  console.log(location.pathname, "pathname");
-  a = location.pathname && location.pathname.includes(value.href);
+
+
+  gIndex = null;
   gIndex = index;
-  console.log(a, 'includes');
 };
+
 
 const SubNavigation = (props) => {
   if (props.isHelp) {
-    const location = window.location;
     return (
       <div className="sub-navigation-custom-help sub-nav-sticky">
         <nav>
@@ -27,17 +24,22 @@ const SubNavigation = (props) => {
                     <span>{value.name}</span>
                   </Link>
                 </li>
+
+
                 {value.subItems.map((v, i) => {
-                  if (a && gIndex === index) {
-                  return <li
-                    className="sub-navigation__item"
-                    key={i} style={{ paddingLeft: "30px" }}>
-                    <Link activeClassName="sub-navigation__item--active"
-                          className="govuk-link subnav" to={v.href}>
-                      <span style={{ color: "#005ea5" }}>{v.name}</span>
-                    </Link>
-                  </li>;
-                } else { return <div key={i}/> }}) }
+                  if (gIndex === index) {
+                    return <li
+                      className="sub-navigation__item"
+                      key={i} style={{ paddingLeft: "30px" }}>
+                      <Link partiallyActive={true} activeClassName="sub-navigation__item--active"
+                            className="govuk-link subnav" to={v.href}>
+                        <span style={{ color: "#005ea5" }}>{v.name}</span>
+                      </Link>
+                    </li>;
+                  }
+                })}
+
+
               </div>;
             })}
           </ol>
--- src/navItems/help-nav-items.js
+++ src/navItems/help-nav-items.js
@@ -7,10 +7,14 @@ export const navItems = [
   },
   {
     name: "Mandatory Declaration", href: "/help/mandatory-declaration/overview/",
-    subItems: [{ name: "Overview", href: "/help/mandatory-declaration/overview/" }]
+    subItems: [{ name: "Overview", href: "/help/mandatory-declaration/overview/" },
+      { name: "Page 2", href: "/help/mandatory-declaration/page-2/" },
+      { name: "Page 3", href: "/help/mandatory-declaration/page-3/" }]
   },
   {
     name: "Immediate Event Notifications", href: "/help/immediate-event-management/overview/",
-    subItems: [{ name: "Overview", href: "/help/immediate-event-management/overview/" }]
+    subItems: [{ name: "Overview", href: "/help/immediate-event-management/overview/" },
+      { name: "Page 2", href: "/help/immediate-event-management/page-2/" },
+      { name: "Page 3", href: "/help/immediate-event-management/page-3/" }]
   }
 ];
