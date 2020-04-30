5c0f5d277f184262367a93b1978fe4aaf14efe04
Dayam Javed <dayam.javed@digital.homeoffice.gov.uk>
2020-04-30 14:43:15 +0100

    changes

---
+++ src/navItems/about-nav-items.js
@@ -0,0 +1,9 @@
+export const navItems = [
+  { name: "About COP", href: "/about/" },
+  {
+    name: "How COP helps you", href: "/about/benefits/"
+  },
+  { name: "Get started", href: "/about/get_started/" },
+  { name: "Roadmap", href: "/about/roadmap/" },
+  { name: "Security", href: "/about/security/" }
+];
---
+++ src/navItems/header-nav-items.js
@@ -0,0 +1,7 @@
+export const navItems = [
+  { name: "Home", href: "/" },
+  { name: "About", href: "/about/" },
+  { name: "Help", href: "/help/oar/overview/" },
+  { name: "Support", href: "/support/" },
+  { name: "Sign-in", href: "https://www.cop.homeoffice.gov.uk/" }
+];
