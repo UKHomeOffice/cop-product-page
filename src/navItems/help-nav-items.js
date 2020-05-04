af8e1274d8676176ec4f834dcf79f544340cb544
Dayam Javed <dayam.javed@digital.homeoffice.gov.uk>
2020-05-04 13:58:48 +0100

    comment out unused page

--- src/navItems/help-nav-items.js
+++ src/navItems/help-nav-items.js
@@ -8,11 +8,11 @@ export const navItems = [
   {
     name: "Accessing your services", href: "/help/accessing/",
     subItems: [{ name: "Create your COP account", href: "/help/accessing/" }]
-  },
-  {
-    name: "Mandec", href: "/help/mandec/",
-    subItems: [{ name: "Overview", href: "/help/mandec/" },
-      { name: "Page-2", href: "/help/mandec/mandec-page-2/" }
-    ]
   }
+  // {
+  //   name: "Mandec", href: "/help/mandec/",
+  //   subItems: [{ name: "Overview", href: "/help/mandec/" },
+  //     { name: "Page-2", href: "/help/mandec/mandec-page-2/" }
+  //   ]
+  // }
 ];
