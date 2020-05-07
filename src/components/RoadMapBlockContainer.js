c6c356f35f5c9bb3967cddd45e90ab590d999825
Dayam Javed <dayam.javed@digital.homeoffice.gov.uk>
2020-05-07 18:50:08 +0100

    roadmap mobile

--- src/components/RoadMapBlockContainer.js
+++ src/components/RoadMapBlockContainer.js
@@ -1,12 +1,16 @@
 import React from "react";
 import RoadMapBlock from "./roadmap-block";
 
-const RoadMapBlockContainer = (props) => (
+const RoadMapBlockContainer = (props) => {
+  console.log(props.content)
+  return (
+
   <div>
     {props.content.map((value, index) => {
       return <RoadMapBlock key={index} text={value} bannerColour={props.bannerColour} />
     })}
   </div>
 );
+}
 
 export default RoadMapBlockContainer;
--- src/components/accordion.js
+++ src/components/accordion.js
@@ -4,11 +4,11 @@ const Accordion = (props) => {
 
 
   return (
-    <div className="accordion">
+    <div style={{marginBottom: '2em'}} className="accordion">
       {props.content.map((value, index) => {
         return <div key={index} className="option">
-          <input type="checkbox" id={index} className="toggle"/>
-          <label style={{fontSize: '16px'}} className="title govuk-heading-s" htmlFor={index}>{value.header}
+          <input type="checkbox" id={`${value.id}${value.header}${index}`} className="toggle"/>
+          <label style={{fontSize: '16px'}} className="title govuk-heading-s" htmlFor={`${value.id}${value.header}${index}`}>{value.header}
           </label>
           <div className="content">
             {value.body}
