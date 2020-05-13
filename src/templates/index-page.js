7dd0421199a4612a46fb26be04f74c02ce26ce39
Dayam Javed <dayam.javed@digital.homeoffice.gov.uk>
2020-05-13 11:49:52 +0100

    changes

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link, graphql } from "gatsby";
-import Layout from "../components/Layout";
+import Layout from "../components/layout";
 import BreakLine from "../components/Break-line";
 import StatContent from "../components/stats-content";
 import MediumTitle from "../components/MediumTitle";
