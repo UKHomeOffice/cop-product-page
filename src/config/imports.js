e7f06bfcc4dc73ed1b41b6c8226fce240ff287fb
Dayam Javed <dayam.javed@digital.homeoffice.gov.uk>
2020-05-14 13:07:05 +0100

    remove imports and add one imported file to all content pages

---
+++ src/config/imports.js
@@ -0,0 +1,50 @@
+import Accordion from "../components/accordion";
+import BoxIcon from "../components/box-icon";
+import BreakLine from "../components/Break-line";
+import BulletList from "../components/bullet-list";
+import { Button } from "../components/button";
+import ExtraLargeTitle from "../components/extra-large-title";
+import Hr from "../components/hr";
+import HyperLink from "../components/hyperlink";
+import Image from "../components/image";
+import LargeParagraph from "../components/large-paragraph";
+import LargeTitle from "../components/large-title";
+import Logo from "../components/logo";
+import MediumTitle from "../components/MediumTitle";
+import Paragraph from "../components/paragraph";
+import RoadMapBlock from "../components/roadmap-block";
+import RoadMapLabel from "../components/roadmap-labels";
+import RoadMapBlockContainer from "../components/RoadMapBlockContainer";
+import SmallTitle from "../components/SmallTitle";
+import Video from "../components/video";
+import StatContent from "../components/stats-content";
+import SubPanel from "../components/sub-panel";
+import layout from '../components/layout'
+import Panel from "../components/panel"
+
+export {
+  Accordion,
+  BoxIcon,
+  BreakLine,
+  BulletList,
+  Button,
+  ExtraLargeTitle,
+  Hr,
+  HyperLink,
+  Image,
+  LargeParagraph,
+  LargeTitle,
+  Logo,
+  MediumTitle,
+  Paragraph,
+  RoadMapBlock,
+  RoadMapLabel,
+  RoadMapBlockContainer,
+  SmallTitle,
+  Video,
+  StatContent,
+  SubPanel,
+  layout,
+  Panel
+}
+
